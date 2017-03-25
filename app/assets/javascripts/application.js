c6197472814158ca541853326fe61244bc5486aa
Reykuizon <reykuizon@gmail.com>
2017-03-25 16:12:08 +0100

    edited background and text

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,10 +35,10 @@ function renderTime(){
   var hour = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirten","Fourthen","Fifteen","Sixten","Seventeen","Eighteen","Nineteen","Twenty","Twenty-One","Twenty-Two","Twenty-Three"] ;
 
   // ASSIGN ARRAY FOR MINUTES
-  var minutes = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirten","Fourthen","Fifteen","Sixten","Seventeen","Eighteen","Nineteen","Twenty","Twenty-One","Twenty-Two","Twenty-Three","Twentey-Four","Twentey-Five","Twentey-Six","Twentey-Seven","Twentey-Eight","Twentey-Nine","Thirty","Thirty-One","Thirty-Two","Thirty-Three","Thirty-Four","Thirty-Five","Thirty-Six","Thirty-Seven","Thirty-Eight","Thirty-Nine","Forty","Forty-One","Forty-Two","Forty-Three","Forty-Four","Forty-Five","Forty-Six","Forty-Seven","Forty-Eight","Forty-Nine","Fifty","Fifty-One","Fifty-Two","Fifty-Three","Fifty-Four","Fifty-Five","Fifty-Six","Fifty-Seven","Fifty-Eight","Fifty-Nine" ] ;
+  var minutes = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirten","Fourthen","Fifteen","Sixten","Seventeen","Eighteen","Nineteen","Twenty","Twenty-1","Twenty-2","Twenty-3","Twenty-4","Twenty-5","Twenty-6","Twenty-7","Twenty-8","Twenty-9","Thirty","Thirty-1","Thirty-2","Thirty-3","Thirty-4","Thirty-5","Thirty-6","Thirty-7","Thirty-8","Thirty-9","Forty","Forty-1","Forty-2","Forty-3","Forty-4","Forty-5","Forty-6","Forty-7","Forty-8","Forty-9","Fifty","Fifty-1","Fifty-2","Fifty-3","Fifty-4","Fifty-5","Fifty-6","Fifty-7","Fifty-8","Fifty-9" ] ;
   
   // ASSIGN ARRAY FOR SECONDS
-  var seconds = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirten","Fourthen","Fifteen","Sixten","Seventeen","Eighteen","Nineteen","Twenty","Twenty-One","Twenty-Two","Twenty-Three","Twentey-Four","Twentey-Five","Twentey-Six","Twentey-Seven","Twentey-Eight","Twentey-Nine","Thirty","Thirty-One","Thirty-Two","Thirty-Three","Thirty-Four","Thirty-Five","Thirty-Six","Thirty-Seven","Thirty-Eight","Thirty-Nine","Forty","Forty-One","Forty-Two","Forty-Three","Forty-Four","Forty-Five","Forty-Six","Forty-Seven","Forty-Eight","Forty-Nine","Fifty","Fifty-One","Fifty-Two","Fifty-Three","Fifty-Four","Fifty-Five","Fifty-Six","Fifty-Seven","Fifty-Eight","Fifty-Nine" ] ;
+  var seconds = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirten","Fourthen","Fifteen","Sixten","Seventeen","Eighteen","Nineteen","Twenty","Twenty-1","Twenty-2","Twenty-3","Twenty-4","Twenty-5","Twenty-6","Twenty-7","Twenty-8","Twenty-9","Thirty","Thirty-1","Thirty-2","Thirty-3","Thirty-4","Thirty-5","Thirty-6","Thirty-7","Thirty-8","Thirty-9","Forty","Forty-1","Forty-2","Forty-3","Forty-4","Forty-5","Forty-6","Forty-7","Forty-8","Forty-9","Fifty","Fifty-1","Fifty-2","Fifty-3","Fifty-4","Fifty-5","Fifty-6","Fifty-7","Fifty-8","Fifty-9" ] ;
   
   // GET AND SET MONTHS
   $(".month").html(months[mn.getMonth()] +" " +dt.getDate()+", "+y.getFullYear());
@@ -48,14 +48,14 @@ function renderTime(){
   $(".hour").html(hour[h.getHours()]+" hundred : "); 
 
   // CHECK FOR PLURAL OR SINLGULAR NOUN TO USE FOR MINUTES
-  if (m.getMinutes()==0) {
+  if (m.getMinutes()<=1) {
     $(".minute").html(minutes[m.getMinutes()]+" minute ");   
   } else {
     $(".minute").html(minutes[m.getMinutes()]+" minutes "); 
   };
 
   // CHECK FOR PLURAL OR SINLGULAR NOUN TO USE FOR SECONDS
-  if (s.getSeconds()==0) {
+  if (s.getSeconds()<=1) {
     $(".second").html(seconds[s.getSeconds()]+" second");   
   } else {
      $(".second").html(seconds[s.getSeconds()]+" seconds"); 
