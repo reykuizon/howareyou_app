7d0067f0eedaf78950712faa510c046d698a6511
Reykuizon <reykuizon@gmail.com>
2017-03-25 22:04:07 +0100

    change to greetings style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,36 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+ 
+$(document).ready(function(){
+  var name;
+  var h = new Date();
+  name = prompt ("What is your name?"); //POP UP QUESTION FOR NAME
+
+  
+  // CHECK FOR IF GOOD MORNING
+  if (h.getHours()<12){ 
+    $(".greetings").html("Good morning, " + name.toUpperCase()+ ".");
+    $(".how_are_you").html("How's your morning?");  
+}
+  
+  // CHECK IF GOOD AFTERNOON
+  if (h.getHours()>=12){
+    $(".greetings").html("Good afternoon, " + name.toUpperCase()+ ".");
+    $(".how_are_you").html("How's your day?");  
+  };
+  
+  // CHECK IF GOOD EVENING
+  if (h.getHours()>=18){
+    $(".greetings").html("Good evening, " + name.toUpperCase()+ ".");
+    $(".how_are_you").html("How's your evening?");  
+  };
+  
+});
+ 
 
 function renderTime(){
-  // SET VARIABLSE FOR DATE
+  // SET VARIABLSE FOR DATES
   var d = new Date();
   var h = new Date();
   var m = new Date();
@@ -24,43 +51,24 @@ function renderTime(){
   var y = new Date();
   var mn = new Date();
   var dt = new Date();
-
+ 
   // ASSIGN ARRAY FOR MONTHS
   var months = ["January","February","March","April","May","June","July","August","Septempber","October","November","December"];
 
   // ASSIGN ARRAY FOR DAYS
   var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   
-  // ASSIGN ARRAY FOR HOURS
-  var hour = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirten","Fourthen","Fifteen","Sixten","Seventeen","Eighteen","Nineteen","Twenty","Twenty-One","Twenty-Two","Twenty-Three"] ;
-
-  // ASSIGN ARRAY FOR MINUTES
-  var minutes = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirten","Fourthen","Fifteen","Sixten","Seventeen","Eighteen","Nineteen","Twenty","Twenty-1","Twenty-2","Twenty-3","Twenty-4","Twenty-5","Twenty-6","Twenty-7","Twenty-8","Twenty-9","Thirty","Thirty-1","Thirty-2","Thirty-3","Thirty-4","Thirty-5","Thirty-6","Thirty-7","Thirty-8","Thirty-9","Forty","Forty-1","Forty-2","Forty-3","Forty-4","Forty-5","Forty-6","Forty-7","Forty-8","Forty-9","Fifty","Fifty-1","Fifty-2","Fifty-3","Fifty-4","Fifty-5","Fifty-6","Fifty-7","Fifty-8","Fifty-9" ] ;
-  
-  // ASSIGN ARRAY FOR SECONDS
-  var seconds = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirten","Fourthen","Fifteen","Sixten","Seventeen","Eighteen","Nineteen","Twenty","Twenty-1","Twenty-2","Twenty-3","Twenty-4","Twenty-5","Twenty-6","Twenty-7","Twenty-8","Twenty-9","Thirty","Thirty-1","Thirty-2","Thirty-3","Thirty-4","Thirty-5","Thirty-6","Thirty-7","Thirty-8","Thirty-9","Forty","Forty-1","Forty-2","Forty-3","Forty-4","Forty-5","Forty-6","Forty-7","Forty-8","Forty-9","Fifty","Fifty-1","Fifty-2","Fifty-3","Fifty-4","Fifty-5","Fifty-6","Fifty-7","Fifty-8","Fifty-9" ] ;
-  
   // GET AND SET MONTHS
   $(".month").html(months[mn.getMonth()] +" " +dt.getDate()+", "+y.getFullYear());
+
   // GET AND SET DATE
   $(".day").html(days[d.getDay()]);
-  // GET AND SET HOURS
-  $(".hour").html(hour[h.getHours()]+" hundred : "); 
+  
+  // GET AND SET TIME
+  $(".hour").html(h.getHours()+":"); 
+  $(".minute").html(m.getMinutes() +":");
+  $(".second").html(s.getSeconds()); 
 
-  // CHECK FOR PLURAL OR SINLGULAR NOUN TO USE FOR MINUTES
-  if (m.getMinutes()<=1) {
-    $(".minute").html(minutes[m.getMinutes()]+" minute ");   
-  } else {
-    $(".minute").html(minutes[m.getMinutes()]+" minutes "); 
-  };
-
-  // CHECK FOR PLURAL OR SINLGULAR NOUN TO USE FOR SECONDS
-  if (s.getSeconds()<=1) {
-    $(".second").html(seconds[s.getSeconds()]+" second");   
-  } else {
-     $(".second").html(seconds[s.getSeconds()]+" seconds"); 
-  };
- 
   // SET TIMEOUT TO 1 SECOND
   setTimeout("renderTime()",1000);
 };
